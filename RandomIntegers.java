9960602638f0b61b841366447863049d49609a74
Elvis Chinaka <elvisuchechi@gmail.com>
2020-02-17 11:51:51 +0100

    i just create this project

---
+++ RandomIntegers.java
@@ -0,0 +1,11 @@
+ // RandomIntegers.java
+ // shifted and scaled rondom integers.
+ import java util.Random; // program uses class random
+
+ public class RandomIngtegers
+ {
+     public static void main( String[] args )
+     {
+         Rand
+     }
+ }
