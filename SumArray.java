e0d025b3bc9a65c04cf1cc575347dd03643bddaf
Elvis Chinaka <elvisuchechi@gmail.com>
2020-02-19 13:32:08 +0100

    add recent files

--- SumArray.java
+++ SumArray.java
@@ -1,2 +1,18 @@
   // SumArray.java
-  //
+  // Computing the sum of element of an array.
+
+  public class SumArray
+  {
+      public static void main( String[] args )
+      {
+        int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87, };
+        int total = 0;
+
+        // add each element's value to total
+        for ( int counter = 0; counter < array.length; counter++ )
+        total += array[ counter];
+
+        System.out.printf( "Total of array elemrnt: %d\n", total );
+      } // end main
+  } // end class SumArray
+
